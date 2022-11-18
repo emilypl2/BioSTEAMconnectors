6455270c13428cfc927cc8aee992e1642bcf789b
Yalin Li <mailto.yalin.li@gmail.com>
2022-11-17 20:36:28 -0600

    finish adding values in "Parameters" sheet

--- BioSTEAMconnectors/_fdcic.py
+++ BioSTEAMconnectors/_fdcic.py
@@ -8,11 +8,8 @@
 # for license details.
 
 
-import os, pandas as pd
+from math import e
 from thermosteam.units_of_measure import AbsoluteUnitsOfMeasure as auom
-
-folder = os.path.dirname(__file__)
-
 
 class Var:
     '''
@@ -210,6 +207,22 @@ default_parameters = [
     Var('EFc', 0.65, 'kg CH4/ha/day'),
     Var('Rice_cultivation_period', 139, 'day'),
     Var('Rice_straw_application_rate', 4, 'Mg/ha'),
+    ### Rye cover crop: yield, N content in biomass, and N2O Emission ###
+    Var('RyeCCfarming_biomass_N2O_factor', 0.01264, 'g N2O-N/g-N'),
+    Var('RyeCCfarming_Ncontent', 0.016, 'ton-N/dry ton-cover crop'),
+    ### Manure N and P2O5 content ###
+    Var('Swine_manure_N', 1279.1304834, 'g N/ton manure'),
+    Var('Dairy_manure_N', 1950.447191, 'g N/ton manure'),
+    Var('Cattle_manure_N', 1991.2705043, 'g N/ton manure'),
+    Var('Chicken_manure_N', 7302.837157, 'g N/ton manure'),
+    Var('Manure_transportation_energy_ton_mile', 10416.49299, 'Btu/ton manure/mile'),
+    Var('Manure_transportation_distance_mile', 0.367, 'mile'),
+    Var('Manure_N2O_factor', 0.01474, 'g-N2O-N/g-N applied'),
+    ### Global warming potential ###
+    Var('CO2_GWP', 1, 'kg CO2e/kg CO2'),
+    Var('CH4_GWP', 29.8, 'kg CO2e/kg CH4'),
+    Var('N2O_GWP', 273, 'kg CO2e/kg N2O'),
+    Var('Biogenic_CH4_GWP', 28, 'kg CO2e/kg CH4'),
     
     # =========================================================================
     # Sheet "Intermediate"
@@ -224,8 +237,15 @@ default_parameters = [
 
 
 default_inputs = [
-    
-    
+    ### Rice: N content of above and below ground biomass and N2O Emission ###
+    Var('Rice_water_regime_during_cultivation', 'Continuously flooded'),
+    Var('Rice_water_regime_pre_season', 'Non flooded pre-season >365 d'),
+    Var('Rice_time_for_straw_incorporation', 'Straw incorporated shortly (<30 days) before cultivation'),
+    ### Regionalized N2O emission factors table ###
+    Var('Climate_zone', 'No consideration'),
+    Var('Nfertilizer_N2O_factor_US_rice', 0.004+0.00374, notes='direct and indirect'),
+    ### 4R nitrogen management practice for corn farming,  ###
+    Var('N_balance_assumed', 0, 'kg N/ha'),
     ]
 
 # %%
@@ -266,54 +286,67 @@ class FDCIC:
         
     @property
     def g_to_lb(self):
+        '''Same as `g_per_lb`.'''
         return self.g_per_lb
     
     @property
     def btu_to_MJ(self):
+        '''Same as `Btu_per_MJ`.'''
         return self.Btu_per_MJ
     
     @property
     def tonne2lb(self):
+        '''Same as `tonne_per_lb`.'''
         return self.tonne_per_lb
 
     @property
     def kg_to_lb(self):
+        '''Same as `kg_per_lb`.'''
         return self.kg_per_lb
     
     @property
     def lb2bu_CanCorn(self):
+        '''Same as `lb_per_bu_CanCorn`.'''
         return self.lb_per_bu_CanCorn
     
     @property
     def L_to_gal(self):
+        '''Same as `L_per_gal`.'''
         return self.L_per_gal
     
     @property
     def ac_per_ha(self):
+        '''Same as `acre_per_hectare`.'''
         return self.acre_per_hectare
 
     @property
     def kg2g(self):
+        '''Same as `g_per_kg`.'''
         return self.g_per_kg
 
     @property
     def MJ2kJ(self):
+        '''Same as `kJ_per_MJ`.'''
         return self.kJ_per_MJ
     
     @property
     def kWh2kJ(self):
+        '''Same as `kJ_per_kWh`.'''
         return self.kJ_per_kWh
 
     @property
     def GJ2kJ(self):
+        '''Same as `kJ_per_GJ`.'''
         return self.kJ_per_GJ
     
     @property
     def ton2g(self):
+        '''Same as `g_per_ton`.'''
         return self.g_per_ton
 
     @property
     def Urea_content_in_UAN(self):
+        '''Same as `UAN_Prod_UreaIn`.'''
         return self.UAN_Prod_UreaIn
     
     @property
@@ -325,8 +358,13 @@ class FDCIC:
         elif regime == 'Multiple drainage period': return 0.55
         elif regime == 'Regular rainfed': return 0.54
         elif regime == 'Drought prone': return 0.16
-        return 0.06
-    
+        elif regime == 'Deep water': return 0.06
+        raise ValueError(
+            'Invalid input for `Rice_water_regime_during_cultivation`, '
+            'can only be one of "Continuously flooded", "Single drainage period", '
+            '"Multiple drainage period", "Regular rainfed", "Drought prone", or "Deep water", '
+            f'not {regime}.')
+
     @property
     def SFp(self):
         '''Scaling factor to account for the differences in water regime in the pre-season before the cultivation period (SFp).'''
@@ -334,14 +372,24 @@ class FDCIC:
         if regime == 'Non flooded pre-season <180 d': return 1
         elif regime == 'Non flooded pre-season >180 d': return 0.89
         elif regime == 'Flooded pre-season (>30 d)': return 2.41
-        return 0.59
+        elif regime == 'Non-flooded pre-season >365 d': return 0.59
+        raise ValueError(
+            'Invalid input for `Rice_time_for_straw_incorporation`, '
+            'can only be one of "Non flooded pre-season <180 d", "Non flooded pre-season >180 d", '
+            '"Flooded pre-season (>30 d)", or "Non-flooded pre-season >365 d", '
+            f'not {regime}.')
 
     @property
     def Rice_ammendment_factor(self):
         '''Conversion factor for organic amendment in terms of its relative effect with respect to straw applied shortly before cultivation.'''
         app_time = self.Rice_time_for_straw_incorporation
-        return 1 if app_time == 'Straw incorporated shortly (<30 days) before cultivation' else 0.19
-
+        if app_time == 'Straw incorporated shortly (<30 days) before cultivation': return 1 
+        elif app_time == 'Straw incorporated long (>30 days) before cultivation': return 0.19
+        raise ValueError(
+            'Invalid input for `Rice_time_for_straw_incorporation`, '
+            'can only be "Straw incorporated shortly (<30 days) before cultivation", or '
+            '"Straw incorporated long (>30 days) before cultivation", '
+            f'not {app_time}.')
     
     @property
     def SFo(self):
@@ -350,10 +398,40 @@ class FDCIC:
     
     @property
     def Annual_CH4_emission_from_rice_field(self):
+        '''In kg CH4/ha.'''
         return self.EFc*self.SFw*self.SFp*self.Fo*self.Rice_cultivation_period
     
+    @property
+    def Nfertilizer_direct_N2O_factor_US_corn(self):
+        zone = self.Climate_zone
+        if zone in ('No consideration', 'NA', 'Wet or Moist'): return 0.01
+        elif zone == 'Dry': return 0.005
+        raise ValueError(
+            'Invalid input for `Climate_zone`, can only be one of "No consideration", '
+            f'"NA", "Wet or Moist", or "Dry", not {zone}.')
     
+    @property
+    def Nfertilizer_indirect_N2O_factor_US_corn(self):
+        zone = self.Climate_zone
+        if zone in ('No consideration', 'NA'): return 0.00374
+        elif zone == 'Wet or Moist': return 0.00418
+        elif zone == 'Dry': return 0.00055    
     
+    @property
+    def Nfertilizer_N2O_factor_US_corn(self):
+        return self.Nfertilizer_direct_N2O_factor_US_corn+self.Nfertilizer_indirect_N2O_factor_US_corn
+
+    @property
+    def Nitrogen_balance_assumed(self):
+        '''Same as `N_balance_assumed`.'''
+        return self.N_balance_assumed
     
+    @property
+    def Nfertilizer_direct_N2O_4R_US_corn(self):
+        '''N2O-N emissions per bushel of corn under 4R practice, [g GHG/bu].'''
+        return e**(0.339+0.0047*self.N_balance_assumed)
+
+
+        
     
 # %%
